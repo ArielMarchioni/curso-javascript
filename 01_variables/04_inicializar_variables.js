63bf2b141e797a314cf4b7b8e30ead222ae405ea
ArielMarchioni <arielmarchioni@gmail.com>
2022-11-25 09:56:18 -0300

    Aprendo a iniciar variables

---
+++ 01_variables/04_inicializar_variables.js
@@ -0,0 +1,19 @@
+//Inicializar variables
+//declarar es solo asi, es solo crearla 
+var x ; //declarada
+
+//inicialiazar es crear y dar valor 
+
+var x 9; //inicalizada
+
+var miIdioma = "Español" //inicializada
+
+
+//Variables no inicializadas . sin va lor asignado
+
+var a ;// NO inicializada
+var b ;// NO inicializada
+console.log(a) // Undifided
+
+a = 56; // le agrego un valor
+console.log(a) // devuelve 56
