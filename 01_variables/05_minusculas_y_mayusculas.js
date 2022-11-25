a89ff70f9dd3faa5f3c47ace873634c227eb49c4
ArielMarchioni <arielmarchioni@gmail.com>
2022-11-25 16:41:37 -0300

    diferencias entre mayusculas y minusculas

---
+++ 01_variables/05_minusculas_y_mayusculas.js
@@ -0,0 +1,12 @@
+// MAYUSCULAS y minusculas
+
+en javascrip se discrimina estre MAYUSCULAS y minusculas
+
+var miVariable = 5 ;
+
+console.log(MIVARIABLLE)//ERROR por que no es igual el nombre la variable
+
+console.log(Mivariable)// ERROR
+
+//PARA QUE FUNCIONE TIENE QUE ESTAR ESCRITRA DE LA MISMA FORMA
+console.log(miVariable)
