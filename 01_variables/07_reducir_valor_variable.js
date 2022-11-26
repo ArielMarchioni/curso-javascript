9383438bca9b2d5825c8c7a9ea63ed4e68710b0e
ArielMarchioni <arielmarchioni@gmail.com>
2022-11-26 11:27:00 -0300

    reducimos el valor de una variable

---
+++ 01_variables/07_reducir_valor_variable.js
@@ -0,0 +1,13 @@
+// reducir el valor actual de la variable en 1 
+
+var numeroDeEstudiantes = 256;
+console.log(numeroDeEstudiantes);
+
+// opcion 1 reducir valor en 1
+numeroDeEstudiantes = numeroDeEstudiantes - 1;
+console.log(numeroDeEstudiantes);
+
+//opcion 2
+
+numeroDeEstudiantes--;
+console.log(numeroDeEstudiantes);
