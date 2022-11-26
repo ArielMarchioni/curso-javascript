27760f1feefb02a5acfbc595c492dfc240a5dc91
ArielMarchioni <arielmarchioni@gmail.com>
2022-11-26 11:26:07 -0300

    Aprendemos a incrementar el valor de una variable

---
+++ 01_variables/06_incrementar_valor_variable.js
@@ -0,0 +1,12 @@
+var librosComprados = 105
+console.log(librosComprados);
+
+//opcion 1 
+
+librosComprados = librosComprados + 1;
+console.log(librosComprados);// 106
+
+//opcion 2 mas usada mejor 
+
+librosComprados++;// "++" incrimenta el valor de 1 en 1 
+console.log(librosComprados);
