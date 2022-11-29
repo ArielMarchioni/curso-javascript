ad8dd562dcfa4472fc3ade1755d3ec1ee03efcff
ArielMarchioni <arielmarchioni@gmail.com>
2022-11-29 11:26:07 -0300

    Asignacion de resta

---
+++ 01_variables/09_asignacion_de_resta.js
@@ -0,0 +1,18 @@
+// reducir el valor de una variable
+
+var a = 23;
+a = a -3;
+console.log(a)
+//opcion 2
+
+var a -= 3;
+console.log(a);
+
+//ejemplos
+
+var salario = 2446;
+
+
+salario -= 5;
+console.log(salario);
+
