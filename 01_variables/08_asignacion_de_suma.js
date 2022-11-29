ae5c0935973827b4e18d7937638756b169019206
ArielMarchioni <arielmarchioni@gmail.com>
2022-11-29 11:23:08 -0300

    Asignacion de suma

---
+++ 01_variables/08_asignacion_de_suma.js
@@ -0,0 +1,12 @@
+//podemos incrementar o reducir las variables en cualquier numeros que queramos
+var a = 23;
+//queremos aumentar en 5 a la variable
+a + a + 5;
+console.log(a);
+
+
+//2 opcion 
+
+a += 5;
+console.log(a);
+
