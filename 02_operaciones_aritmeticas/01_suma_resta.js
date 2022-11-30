47c2d0e97abd87ab71926d04873d0ee1f5f967e0
ArielMarchioni <arielmarchioni@gmail.com>
2022-11-30 19:31:17 -0300

    operaciones con sumas y restas

---
+++ 02_operaciones_aritmeticas/01_suma_resta.js
@@ -0,0 +1,16 @@
+//SUMA
+
+var suma = 7 + 12;//en la variable suma se guarda el valor de 7 + 12
+
+console.log(suma);// devuelve 19
+
+//RESTA
+
+var resta = 15 - 5;
+console.log(resta);//10
+
+resta = 5 - 15 ;//asignamos otro valor a la variable 
+.console.log(resta);// -10
+
+resta = 15 - 15;
+console.log(resta); // 0
