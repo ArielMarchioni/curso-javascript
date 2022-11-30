94e3654cad8fb7f5d7ca187cfa1ae7eded4fe9bd
ArielMarchioni <arielmarchioni@gmail.com>
2022-11-30 19:29:29 -0300

    asignamos con multiplicaciones

---
+++ 01_variables/10_asignacion_de_multiplicacion.js
@@ -0,0 +1,35 @@
+// aumentar mutiplicacion
+
+var c = 23;
+
+c = c * 2;
+console.log(c);
+
+//opcion 2 
+
+c *= 2;
+console.log(c);
+
+//ejemplo quiero multiplicar mi salario por 5
+
+var salario = 45000;
+salario *= 5;
+console.log(salario);
+
+// Division
+
+var d = 39;
+d = d / 3;
+console.log(d);
+
+//opcion 2 
+
+var e = 39;
+e /= 3;
+console.log(e);
+
+//ejemplo van a reducirme el salio a la mitad :(
+
+var salario = 45000;
+salario /= 2;
+console.log(salario);
