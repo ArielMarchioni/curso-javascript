cae4b5c5ecdecbce2fb638fba86cb1a7d3e4d43e
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-01 08:12:06 -0300

    aprendo a multiplicar y dividir

---
+++ 02_operaciones_aritmeticas/02_multiplicacion_division.js
@@ -0,0 +1,22 @@
+// MULTIPLICACION
+
+var producto = 5 * 3 ;
+console.log(producto);// 15
+
+producto = 9 * 0;
+console.log(producto);//0
+
+producto= -5 * -6;
+console.log(producto);//30
+
+
+//DIVISION
+
+var cociente = 20 / 2;
+console.log(cociente); //enteros 
+
+cociente = 17 / 31;//se puede representar numeros
+console.log(cociente); //numeros decimales
+
+cociente = 3 / 0; 
+console.log(cociente);//develve INFINITY (wtf)
