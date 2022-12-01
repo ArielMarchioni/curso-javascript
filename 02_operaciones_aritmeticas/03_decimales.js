3f3adb83a0b6a11e6d6f24de54ecd62464e4f623
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-01 08:12:45 -0300

    operacione con decimales

---
+++ 02_operaciones_aritmeticas/03_decimales.js
@@ -0,0 +1,25 @@
+//NUMEROS DECIMALES
+
+var miNumeroDecimal = 23.4 //SE REPRESENTA EL NUMEORO CON UN . 
+
+var peso = 78.34;
+
+
+/*OPERAR CON NUMEROS DECIMALES
+ es lo mismo XD
+MULTIPLICACION
+ */
+var producto = 3.4 * 10.4;
+console.log(producto);
+
+producto 6 * 8.9;
+console.log(producto);
+
+//DIVISION
+var cociente = 5.0 / 2.0;
+console.log(cociente);
+
+cociente = 2.3 / 6.7;
+console.log(cociente);
+
+
