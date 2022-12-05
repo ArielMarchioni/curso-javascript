af0666bad8787a74a7e91c3a36fa8ccd9262a172
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-05 12:55:40 -0300

    resto de division

---
+++ 02_operaciones_aritmeticas/04_resto_de_division.js
@@ -0,0 +1,7 @@
+//RESTO DE DIVISION , UTIL SI QUEREMOS SABER SI UN NUMERO ES PAR O INPAR
+
+var resto = 15 % 5; //se obtiene el resto con el simbolo "%"
+console.log(resto);
+
+resto = 5 % 2;
+console.log(resto);//1
