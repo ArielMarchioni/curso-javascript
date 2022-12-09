576a6e7ab4fb53cc5dc55a1a8ab59bbe86dcbcec
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-09 10:58:23 -0300

    Usamos strings en Js

---
+++ 03_strings/01_strings.js
@@ -0,0 +1,19 @@
+
+//STRINGS , SON CADENAS DE CARACTERES , ENTRE COMILLAS "STRING ",
+//LOS ESPACIOS TAMBIEN SON CARACTERES. 
+
+var nombre = "Ariel Gerardo"
+
+var apellido = "Marchioni"
+console.log(nombre,apellido)
+
+/*no se puede poner comillas iguales dentro de un string
+ se una barra invertida antes de las comillas para decirle que son parte del string*/
+var miCadena = "Soy una cadena de caracteres con \"comillas\""\
+
+
+//cadenas de caracteres con comillas simples
+
+var miMeta;
+
+miMeta = "Tener casa `propia´"
---
+++ 03_strings/02_secuencias_de_escape.js
@@ -0,0 +1,19 @@
+/* Secuencias de Escape
+
+codigo | Resultado
+-------------------
+\'       Comilla simple
+\"		 Comilla doble	
+\\		barra invertida
+\ 		linea mueva
+\ 		retorno carro
+\t 		tabulacion
+\b 		retroceso
+\f 		salto de página
+
+*/
+
+console.log("estoy aprendiendo //Javascript"); 
+
+console.log("estoy aprendiendo: /nJavascript"); // muestra en otra linea
+
