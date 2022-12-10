362ecf4bb5fec6df22c3e5ceb34a01ac68a1bdab
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-10 11:30:22 -0300

    Concatenamos las cadenas de caracteres

---
+++ 03_strings/03_concatenar_cadenas_de_caracteres.js
@@ -0,0 +1,35 @@
+/*Unir cadenas de caracteres
+con operador +
+
+*/
+
+var nombreCompleto = "Ariel" + "Marchioni"
+console.log(nombreCompleto); //devuelve el valor pero sin espacios
+
+nombreCompleto = "Ariel" " " "Marchioni"
+console.log(nombreCompleto); //muesta el espacio 
+
+
+
+//Construir candedas con variables.
+
+var verbo = "programar"
+
+var mensaje = "Estoy aprendiendo a " + verbo;
+console.log(mensaje);//
+
+var mensaje = "Estoy aprendiendo a " + verbo + " con facilidad";
+console.log(mensaje);
+
+
+
+//agregar variables con cadenas de caracteres
+
+var mensajeCompleto = "Estoy aprendiendo a programar "
+var parteFinal = "con facilidad"
+
+console.log(mensajeCompleto);
+
+mensajeCompleto += parteFinal
+console.log(nombreCompleto);
+
---
+++ 03_strings/04_longitud_cadena_caracteres.js
@@ -0,0 +1,11 @@
+//Encontrar longitud de una cadena
+
+var miCadena;
+
+miCadena = "A"
+
+//se usa ".length" para saber la longitud de la cadena
+console.log(miCadena.length); //devuelve 1
+
+miCadena = "JavaScript"
+console.log(miCadena.length); devuelve 10
