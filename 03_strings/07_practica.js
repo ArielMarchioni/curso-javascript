71007125998a01b2da8298c31e4fd6fc8d99d40d
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-12 11:15:08 -0300

    Practicamos lo que aprendimos

---
+++ 03_strings/07_practica.js
@@ -0,0 +1,18 @@
+var miSustantivo = "perro"
+var miAdjetivo = "negro"
+var miVerbo = "corrió"
+var MiAdvervio = "rápidamente"
+
+/*
+Concaneta las cadenas para crear una cadena con el siguiente mensaje:
+El perro negro corrió rápidamente a la tienda
+
+*/
+
+var palabrasEnBlanco = "El" + miSustantivo + miAdjetivo + miVerbo + miAdjetivo + "a la tienda."
+console.log(palabrasEnBlanco)//Elperronegrocorriórápidamentea la tienda
+
+//Agregamos los espacios
+
+palabrasEnBlanco = "El" + " " +miSustantivo + " " + miAdjetivo +" "+ miVerbo + " " + miAdjetivo " "+ "a la tienda."
+
