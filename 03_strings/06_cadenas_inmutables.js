2a3093617090eb4a77901976c2afd4c53da6a2e1
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-12 11:14:13 -0300

    cademas inmutalbes

---
+++ 03_strings/06_cadenas_inmutables.js
@@ -0,0 +1,14 @@
+
+//las cadenas son imutables
+
+var miCadena = "Jola mundo"
+console.log(miCadena);
+
+miCadena[0] = "H" //Error , las cadenas son inmutables
+
+//SI se puede reemplaza toda la cadena
+
+console.log(miCadena);
+
+miCadena = "Hola mundo"
+console.log(miCadena);//se cambio toda la cadena
