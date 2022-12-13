e4701f1a07a60f3cfb1c4521d6bf98ecf96eb217
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-13 14:03:39 -0300

    empezamos con array

---
+++ 04_arrays/01_arrays.js
@@ -0,0 +1,11 @@
+//son como  listas se usa []
+
+var miArray = ["John", 24]
+
+console.log(miArray);
+
+var estudiantes = [ "Nora", "Gino", "Emily", "Juan"]
+console.log(estudiantes);
+
+var notas = [ 95, 67, 89, 56];
+console.log(notas);
