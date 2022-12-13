db9bcea7dff940dd3bb9e77a2c694802ec90af25
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-13 14:04:29 -0300

    Anidamos arrays

---
+++ 04_arrays/02_arrays_anidados.js
@@ -0,0 +1,10 @@
+// se pueden anidar los arrays CON [["info"], ["info"]]
+
+var estudiantes = [[ "Nora" 97], ["Gino",78]];
+console.log(estudiantes);
+
+
+var listaDeProductos = [ ["camisa", 5.67, "S134"], ["celulares", "250", "S356"], ["zapatos", "102", "S342"]];
+console.log(listaDeProductos);
+
+
