b73f622d73230cd5a784c4e32bf00089ee92fea8
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-15 21:55:39 -0300

    nuevo

---
+++ 04_arrays/03_acceder_elementos_array.js
@@ -0,0 +1,18 @@
+// Como acceder a los elementos?
+
+var miArray = [10, 20 ,30];
+
+/*
+ array: [10, 20 , 30]
+indice:   0   1    2
+
+*/
+
+
+console.log(miArray[0]); // 10
+console.log(miArray[2]); // 20
+console.log(miArray[3]); // 30
+
+
+var suma = miArray[0] + miArray[1] + miArray[2];
+console.log(suma); // 60
