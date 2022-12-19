1bdca6f9949baeb490a09421bc83a7500c7df4c2
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-19 12:24:00 -0300

    Aprendemos a modificar elementos del array

---
+++ 04_arrays/04_modificar_elementos.js
@@ -0,0 +1,17 @@
+// Como modificar Arrays???
+
+var miArray = [10, 20, 30];
+
+/*elegimos la variable y le ponemos el indice que 
+queremos modificar [0] en este caso*/
+miArray[0] = 40;
+
+console.log(miArray);
+
+miArray[1] = "Hola, lo modifique";
+console.log(miArray);
+
+
+//tambien podemos modificarlo con otro array
+miArray[2] = [11, 12, 13];
+console.log(miArray);
