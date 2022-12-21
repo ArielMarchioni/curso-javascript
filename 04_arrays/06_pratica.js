94b64239ea378df5d0209c358d551011b806bb73
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-21 08:40:56 -0300

    Practicamos lo que aprendimos hasta ahora

---
+++ 04_arrays/06_pratica.js
@@ -0,0 +1,22 @@
+var listaDeCompra = [["cereal", 3], ["leche", 2], ["galletas", 4], ["pan", 5], ["gaseosas", 7], ["pollo", 7] ];
+
+//cereal
+console.log("voy a comprar " + listaDeCompra[0][1] + " unidades de " + listaDeCompra[0][0]);
+
+//leche
+
+console.log("Vot a comprar "+ listaDeCompra[1][1] + " litros de " + listaDeCompra[1][0]);
+
+
+//galletas
+console.log("voy a comprar "+ listaDeCompra[2][1] + "paquetes de "+ listaDeCompra[2][0]);	
+
+//pollo
+console.log("voy a comprar " + listaDeCompra[5][1] + "kilos de " + listaDeCompra[5][0]);
+
+//gaseosas
+console.log("voy a comprar " + listaDeCompra[4][1] + "botellas de " + listaDeCompra[4][0]);
+
+
+//pan
+console.log("voy a comprar " + listaDeCompra[3][1] + "kilos de " + listaDeCompra[3][0]);
