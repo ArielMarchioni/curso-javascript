637f90569b64b84160c13eb19348092c3d6ec1bf
ArielMarchioni <arielmarchioni@gmail.com>
2022-12-22 12:44:47 -0300

    Empezamos con Funciones

---
+++ 05_funciones/01_funciones.js
@@ -0,0 +1,15 @@
+// asi es la estructura de una funcion !
+function mostrarMensaje(){
+	console.log("Hola, mundo!");
+
+}
+
+
+
+//llamamos a la funcion 
+mostrarMensaje();
+mostrarMensaje();
+mostrarMensaje();
+mostrarMensaje();
+mostrarMensaje();
+mostrarMensaje();
