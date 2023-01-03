4b58609ee7f68ff5de018491a2b6505a55831b3e
ArielMarchioni <arielmarchioni@gmail.com>
2023-01-03 20:58:38 -0300

    ambito global

---
+++ 05_funciones/03_ambito_global.js
@@ -0,0 +1,14 @@
+/*
+varible global sepuede acceder usar y modificar en cualquier
+lugar del programa .
+
+*/
+
+
+var mivariableGlobal = 5 ;
+
+console.log(mivariableGlobal);
+
+function miFuncion(){
+	console.log(mivariableGlobal);
+}
