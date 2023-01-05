deb19342a2ae668404a5252aac6f22cf61b3d4d4
ArielMarchioni <arielmarchioni@gmail.com>
2023-01-05 16:57:57 -0300

    indefinidos

---
+++ 05_funciones/07_undefined.js
@@ -0,0 +1,13 @@
+//undefine Omitir la sentencia ()
+
+function sumar(a, b) {
+	console.log( a + b);
+}
+
+console.log(sumar(5, 3));//8 undifined
+/*
+llamamos a la funcion 
+se muestra el valor de la suma
+y despues es valor retornado por llamar a la funcion
+que es Undefined por defecto
+*/
