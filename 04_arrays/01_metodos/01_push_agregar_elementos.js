dc88a3086edd86cfbb943cf07b05341c29f491dd
ArielMarchioni <arielmarchioni@gmail.com>
2023-01-09 18:56:04 -0300

    Push

---
+++ 04_arrays/01_metodos/01_push_agregar_elementos.js
@@ -0,0 +1,9 @@
+//Agregar valor a Array
+var estaciones = ["Invierno", "Otoño", "Primavera"];
+
+//metodo es como una funcion . que podemos llamar para hacer algo con el codigo
+
+//el metodo ".push()" Para agregar un elento al final
+
+estaciones.push("Verano");
+console.log(estaciones); //["Invierno", "Otoño", "Primavera", "Verano"];
