2f7936deaa9158544ac459c2944ae84668a3a584
ArielMarchioni <arielmarchioni@gmail.com>
2023-01-18 21:22:30 -0300

    Aprendo el metodo pop

---
+++ 04_arrays/01_metodos/02_pop_remover_ultimo_elemento.js
@@ -0,0 +1,20 @@
+/* eliminar el ultimo elemento
+
+metodo .pop() --- no lleva ningun argumento en los parentesis
+*/
+
+
+
+var estaciones;
+
+estaciones = ["Invierno", "Otoño", "Primavera", "Verano"];
+//remover el ultimo elemento
+estaciones.pop(); //["Invierno", "Otoño", "Primavera",];
+
+
+//pop tambien lo retorna al elemento , para asignarlo otra variable
+var estacion 
+estacion = estaciones.pop();
+console.log(estaciones);// ["Invierno", "Otoño", "Primavera",]
+console.log(estacion); //  verano
+
