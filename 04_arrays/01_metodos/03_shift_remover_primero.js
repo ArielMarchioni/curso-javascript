8724f72866b5c2d695d05941e73cb7f6b97ef68d
ArielMarchioni <arielmarchioni@gmail.com>
2023-01-19 22:00:47 -0300

    rem,over primerro

---
+++ 04_arrays/01_metodos/03_shift_remover_primero.js
@@ -0,0 +1,6 @@
+//remover el primer elemento de la cadena con shift()
+
+var estaciones = ["Invierno", "Otoño", "Primavera", "Verano"];
+estaciones.shift();
+
+console.log(estaciones);
---
+++ 04_arrays/01_metodos/04_unshift.agregar_primero.js
@@ -0,0 +1,10 @@
+/*
+Agregar un elemento primero con unsift()
+a diferencia de los demas este si tiene que tener un valor
+para saber que agregar
+*/
+
+
+var estaciones = ["Invierno", "Otoño", "Primavera", ;
+estaciones.unshift("Verano");
+console.log(estaciones);
