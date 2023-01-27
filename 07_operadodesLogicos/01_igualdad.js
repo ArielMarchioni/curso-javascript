17836a5923b786b3382618e5e912846cfaa35fa9
ArielMarchioni <arielmarchioni@gmail.com>
2023-01-26 21:09:06 -0300

    Empezamos con Operadores LOgicos

---
+++ 07_operadodesLogicos/01_igualdad.js
@@ -0,0 +1,10 @@
+/*Operador Igualdad*/
+console.log(5==5/*exprecion*/);//true
+
+console.log(5==6);//false
+
+console.log("hola"=="hola");//true
+
+console.log("js"=="hola");//false
+
+/*no se usa para comparar arreglos*/
