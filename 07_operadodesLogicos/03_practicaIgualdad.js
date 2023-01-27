e6ec724255ca40ef13e3c4db6504841fa54d6012
ArielMarchioni <arielmarchioni@gmail.com>
2023-01-27 19:46:47 -0300

    Practicamos igualdad e igualdad estricta

---
+++ 07_operadodesLogicos/03_practicaIgualdad.js
@@ -0,0 +1,19 @@
+var a ;
+var b;
+
+a=5;
+b=5;
+//operador de igualdad
+console.log(a == b);//true
+
+//operador de estricta
+
+console.log(a === b);//true
+
+b= 8;
+console.log(a == b);//false
+console.log(a === b);//false
+
+b="5"
+console.log(a == b);//true
+console.log(a === b);//false
