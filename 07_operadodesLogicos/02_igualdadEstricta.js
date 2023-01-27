dd1a23b61b5c74e0b6eeb2835625d959176de21b
ArielMarchioni <arielmarchioni@gmail.com>
2023-01-27 19:46:07 -0300

    igualdad estricta

---
+++ 07_operadodesLogicos/02_igualdadEstricta.js
@@ -0,0 +1,10 @@
+/*tipo de datos son los mismo*/
+
+console.log(9==9);//true
+
+console.log(9=="9");//true
+
+
+console.log(9===9);//true
+//queremos ver si son el mismo tipo de dato
+console.log(9==="9");//false
