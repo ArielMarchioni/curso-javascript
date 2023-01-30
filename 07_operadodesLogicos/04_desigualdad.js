cae232c3bdd60dd838c82a81b768f42ea1b73d3f
ArielMarchioni <arielmarchioni@gmail.com>
2023-01-30 11:50:41 -0300

    desigualdad

---
+++ 07_operadodesLogicos/04_desigualdad.js
@@ -0,0 +1,11 @@
+//Operador de desigualdad , compara los valores y retorna
+//false si son iguales o true si son distintos
+
+console.log(9!=6); //true
+
+console.log(9!=9);//false
+
+console.log("JavaScript"!="JavaScript");//true
+console.log("javascript"!="JavaScript");//false
+
+//no se puede comparar arreglos . 
