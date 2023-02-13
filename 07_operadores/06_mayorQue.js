b014b11fb8549155365fa28f245783e487eb202a
ArielMarchioni <arielmarchioni@gmail.com>
2023-02-13 09:13:33 -0300

    operardor mayor que

---
+++ 07_operadores/06_mayorQue.js
@@ -0,0 +1,22 @@
+//mayor que >
+
+console.log(6 > 5); //true
+
+console.log (3 > 10); //false
+
+
+//si b viene despues de la A en el abecedario
+console.log("B" > "A");//true
+
+console.log("ABC" > "ABC"); //true
+
+console.log("AB" > "A");// true tiene mas caracteres
+
+///se pueden usar con variables
+
+var a = 15;
+
+var b = 7;
+
+console.log(a > b);
+console.log(b > a );
