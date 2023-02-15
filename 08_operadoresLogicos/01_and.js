2c1fbeceacb0d395f28a8e4af87603557dc225bf
ArielMarchioni <arielmarchioni@gmail.com>
2023-02-15 08:46:30 -0300

    And , operador logico

---
+++ 08_operadoresLogicos/01_and.js
@@ -0,0 +1,12 @@
+// AND && , solo es verdadero cuando ambos son verdaderos
+
+console.log(true && true);
+
+var a = 8;
+
+console.log((a > 5 ) && (a < 10) ); //true
+
+var a = 3 
+console.log((a > 5 ) && (a < 10) ); //false
+
+
