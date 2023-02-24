21ffd113311bf04f692a90926d31ce81f2efd6ac
ArielMarchioni <arielmarchioni@gmail.com>
2023-02-24 16:41:06 -0300

    if ejercicio

--- 09_condicionales/01_if.js
+++ 09_condicionales/01_if.js
@@ -3,3 +3,11 @@ var x = 5
 if (x > 2){
 	console.log("la codicion es verdadera");
 }
+
+var estancion = "Invierno";
+
+if (estancion == "Invierno"){
+	console.log("si, Me encanta el invierno");
+
+}
+
