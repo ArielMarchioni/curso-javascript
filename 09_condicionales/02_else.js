ef6dc2506abe215f4604c3e5492bbf96c647387f
ArielMarchioni <arielmarchioni@gmail.com>
2023-02-27 12:36:22 -0300

    condicional else

---
+++ 09_condicionales/02_else.js
@@ -0,0 +1,15 @@
+
+var x = 5 ;
+if(x < 2){
+	console.log("la condicion es verdadera.");
+} else{
+	console.log("La condicion es falsa." );
+}
+
+var estacion = "invierno"
+
+if (estacion == "Verano"){
+	console.log("Comenzo el verano , podemos ir a la playa");
+} else{
+	console.log ("ya quiero que llegue el verano para ir a la playa")
+}
