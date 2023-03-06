0375600250e043141100494ab8545e9cc138126a
ArielMarchioni <arielmarchioni@gmail.com>
2023-03-06 11:32:06 -0300

    Objetos en Js

---
+++ 10_objetos/01_crear_objetos.js
@@ -0,0 +1,9 @@
+//se define objeto con var , entre {} se escribes las propiedades y valores separada 
+//por : 
+
+var miperro = {
+	"nombre": "gino",
+	"edad": 5,
+	"peso": 6,
+	"raza": "Beagle"
+};
