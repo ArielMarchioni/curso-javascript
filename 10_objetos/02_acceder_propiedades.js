a167f2b6ed0dfdf74270dc7541c5308c31b9f448
ArielMarchioni <arielmarchioni@gmail.com>
2023-03-06 11:33:14 -0300

    ponenos Propiedades

---
+++ 10_objetos/02_acceder_propiedades.js
@@ -0,0 +1,13 @@
+//notacion de punto
+
+var	miPerro = {
+	"nombre": "Gino",
+	"edad" : 5,
+	"peso" : 6,
+	"raza" : "Beagle"
+};
+
+console.log(miPerro.nombre);// Gino
+console.log(miPerro.edad);// 5
+console.log(miPerro.peso);// 6
+console.log(miPerro.raza);// Beagle
