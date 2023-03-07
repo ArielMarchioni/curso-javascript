8d61579ae7ed5162255e9b0cafacc8ec871183ac
ArielMarchioni <arielmarchioni@gmail.com>
2023-03-07 15:55:11 -0300

    acceder a variables

---
+++ 10_objetos/04_acceder_variables.js
@@ -0,0 +1,13 @@
+var resultados = {
+	1: "nora256",
+	2: "gino577",
+	3: "esteban535"
+	4: "vas233"
+};
+
+var posicion = 4 ;
+
+console.log(resultados[posicion]);//vas233
+
+var posicion = 1 ;
+console.log(resultados[posicion]);//nora256
