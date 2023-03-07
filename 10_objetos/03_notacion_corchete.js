10fb17c6ee6f6f4450cbbd68ca063bb4e2b73aaf
ArielMarchioni <arielmarchioni@gmail.com>
2023-03-07 15:54:36 -0300

    Notacion de corchete

---
+++ 10_objetos/03_notacion_corchete.js
@@ -0,0 +1,12 @@
+var	miCuaderno = {
+	"color": "verde",
+	"categoria" : 3,
+	"numero de paginas" : 200,
+	"nuemro de hojas" : 100
+};
+//notacion de punto
+console.log(miCuaderno.color);//verde
+
+
+//notacion de corchete . se accede a propiedades que tienen espacios
+console.log(miCuaderno["numero de paginas"]);//200
