a25b40ec2a4c4d0c69428977e2ac0cfc71bbfccf
ArielMarchioni <arielmarchioni@gmail.com>
2023-03-09 18:14:55 -0300

    Eliminar propiedades

---
+++ 10_objetos/07_eliminar_propiedades.js
@@ -0,0 +1,11 @@
+var curso = {
+	"titulo" : "Aprende JavaScript desde cero",
+	"idioma" : "Español",
+	"duracion" : 30
+};
+
+//borrarmos la propiedad duracion
+
+delete curso.duracion;
+
+console.log(curso);
