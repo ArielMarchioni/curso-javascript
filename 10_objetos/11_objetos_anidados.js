a5012c57ef887d96306acbbf8e2bfa5020a6ec4e
ArielMarchioni <arielmarchioni@gmail.com>
2023-03-13 19:55:42 -0300

    anidamos objetos

---
+++ 10_objetos/11_objetos_anidados.js
@@ -0,0 +1,36 @@
+var miReceta = {
+  "descripcion": "mi postre favorito",
+  "costo": 15.6,
+  "ingredientes": {
+    "masa": {
+      "harina": "100 grs",
+      "sal": "1 cucharadita",
+      "agua": "1 taza"
+    },
+    "cobertura": {
+      "azucar": "120 grs",
+      "chocolate": "4 cucharadas",
+      "mantequilla": "200 grs"
+    }
+  }
+};
+//notacion de punto
+console.log(miReceta.descripcion);
+console.log(miReceta.costo);
+console.log(miReceta.ingredientes);
+
+console.log(miReceta.ingredientes.masa);
+
+console.log(miReceta.ingredientes.masa.harina);
+console.log(miReceta.ingredientes.masa.sal);
+console.log(miReceta.ingredientes.masa.agua);
+
+//notacion de corchete
+
+
+console.log(miReceta.ingredientes.masa[harina]);
+
+console.log(miReceta.ingredientes.masa[harina]);
+
+
+
