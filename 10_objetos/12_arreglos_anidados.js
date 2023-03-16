e3ee076c454190115e568c4141a0445c3caa843f
ArielMarchioni <arielmarchioni@gmail.com>
2023-03-16 20:46:20 -0300

    arreglos anidados

---
+++ 10_objetos/12_arreglos_anidados.js
@@ -0,0 +1,26 @@
+var misPlantas = [
+	{ tipo: "flores",
+	listas: [
+		"rosas",
+		"tulipanes",
+		"dientes de leon"
+		]
+
+	},
+	{
+		tipo: "arboles",
+		listas:[
+			"abeto",
+			"pino",
+			"abedul",
+			]
+	}	
+
+];
+
+var miPrimeraFlor = misPlantas[0].listas[0];
+console.log(miPrimeraFlor);//rosas
+
+var segundoArbol = misPlantas[1].listas.[1];//
+
+console.log(segundoArbol);//pino
