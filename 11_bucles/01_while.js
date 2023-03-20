265ae6d4aea7513741de9b60f8bb52337226e5b8
ArielMarchioni <arielmarchioni@gmail.com>
2023-03-20 16:10:17 -0300

    bucle while

---
+++ 11_bucles/01_while.js
@@ -0,0 +1,24 @@
+var i = 0;
+
+while(i <=3){
+	console.log("Hola mundo");
+	i++;
+};
+
+var miArreglo = []
+var i = 0;
+
+console.log(miArreglo);{
+//agregamos los numeros de 0 a 9
+	miArreglo.push(i);
+	i++
+};
+
+console.log(miArreglo);
+
+var numeros = [2, 3, 4, 5, 6, 7, 8,9];
+//eliminanos los elementos 
+while(numeros.length > 4){
+	numeros.pop();
+}
+console.log(numeros);
