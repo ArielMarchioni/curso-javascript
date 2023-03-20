8af2cb3f7cd05ca60b926b156b17e8b6508551e2
ArielMarchioni <arielmarchioni@gmail.com>
2023-03-20 16:10:47 -0300

    bucle for

---
+++ 11_bucles/02_for.js
@@ -0,0 +1,10 @@
+//cuando sabemos cuantas iteraciones son necesarias
+
+var miArreglo= [];
+
+//for ( iniciar valor de variable; condicion; cono se actializa la variable)
+for (var i = 0; i < 10 ; i++) {
+	miArreglo.push(i);
+}
+
+console.log(miArreglo);
