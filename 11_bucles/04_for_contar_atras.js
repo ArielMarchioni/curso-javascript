c6f9f470d2e313268c6b4b71ade381b4021bd04b
ArielMarchioni <arielmarchioni@gmail.com>
2023-04-17 11:02:15 -0300

    Contar para atras

---
+++ 11_bucles/04_for_contar_atras.js
@@ -0,0 +1,18 @@
+//contar hacia atras con un for
+
+for (var i = 15; i >= 10; i-- ){
+	console.log(i);
+}//14,13,12,11,10
+
+//otra opcion es reducirlo de 2 en 2
+
+for (var i = 15; i >= 10; i-=2) {
+	console.log(i);
+}
+
+//
+
+var arreglo = [];
+for(var i = 10 ; i >0; i -=2){
+	Arreglo.push(i);
+}
