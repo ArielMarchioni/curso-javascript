3c534e62621a2252fb36c90e6601f391122b7eca
ArielMarchioni <arielmarchioni@gmail.com>
2023-04-18 15:04:35 -0300

    ciclos do while

---
+++ 11_bucles/07_do_while.js
@@ -0,0 +1,29 @@
+//while se ejecuta si la condicion es verdadera
+
+var x;
+
+x = 5;
+
+while (x < 10){
+	console.log(x);
+	x++;
+}
+
+//do while, se ejecuta antes de evaluar la ejecucion
+
+do{
+	console.log(x);
+	x++;
+}while (x < 10)
+
+
+//
+
+var a = 16;
+
+do{
+	console.log(a);
+	c++; //17
+}while (a < 10);
+
+//se puede usar para pedir valores y despues evaluarlo con una condicion 
