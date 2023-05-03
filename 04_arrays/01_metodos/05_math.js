5cf4cfa29597575a9097cb46501a4407dab49f92
ArielMarchioni <arielmarchioni@gmail.com>
2023-05-03 19:40:07 -0300

    metodo mAth

---
+++ 04_arrays/01_metodos/05_math.js
@@ -0,0 +1,17 @@
+// decimales aleatorios
+function generarFraccionAleatoria(a) {
+	return Math.random();// retorna valores solo de o a 1 sin incluirlo
+
+	// body...
+}
+/*
+console.log(generarFraccionAleatoria);
+console.log(generarFraccionAleatoria);
+console.log(generarFraccionAleatoria);
+console.log(generarFraccionAleatoria);
+console.log(generarFraccionAleatoria);
+console.log(generarFraccionAleatoria);
+*/
+
+var numeroAleatorio = Math.random();
+console.log(numeroAleatorio);
