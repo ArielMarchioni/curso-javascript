8110c19983824f296b2c858e1616411c8fa24f4a
ArielMarchioni <arielmarchioni@gmail.com>
2023-05-04 19:29:47 -0300

    perseint con base

---
+++ 05_funciones/12_parseInt_con_base.js
@@ -0,0 +1,9 @@
+//binario
+//como es numero binario le pasamos la base para que sepa que lo es 
+// console.log(parseInt("numero binario", base))
+
+console.log(parseInt("110111", 2));
+
+//hexadecimal
+
+console.log(parseInt("3E0A", 16));
