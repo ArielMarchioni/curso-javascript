f72dde89af4e4b00b75d01210cebad8c561695c9
ArielMarchioni <arielmarchioni@gmail.com>
2023-05-04 19:27:17 -0300

    -m Funciones Parseint

---
+++ 05_funciones/11_funcion_parselInt.js
@@ -0,0 +1,20 @@
+//funcion ParselInt convierte cadenas de caracteres al tipo de dato entero
+
+console.log(parseInt("8"));//8
+console.log(parseInt("-32"));// -32
+console.log(parseInt("5.2"));// 5
+
+//
+console.log(parseInt("sdsd"));// NaN no es un numero entero
+
+var a = "5"
+
+var b = "7"
+
+console.log(a + b);//57 | se concatenan los caracteres
+
+
+var a = parseInt("5");
+var b = parseInt("7");
+
+console.log(a + b);//12 | se conviente a enteros y los puede sumar
