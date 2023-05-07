e82b4573072dab5c345698691e86e6a3389abc5d
ArielMarchioni <arielmarchioni@gmail.com>
2023-05-07 13:16:04 -0300

    Multiples operadores ternarios

---
+++ 07_operadores/11_multiplesOperadores_condicionales.js
@@ -0,0 +1,17 @@
+function compararNumeros( a , b ){
+	if (a == b){
+		return " a y b son iguales";
+
+	}else if (a > b){
+		return "a es mayor que b"
+	}else{
+		return "b es mayor que a"
+	}
+}
+
+
+function compararNumeros ( a , b){
+	return a == b ? "a y b son iguales"
+		 : a > b ? " a es mayor que b"
+		 : "b es mayor que a " 
+}
