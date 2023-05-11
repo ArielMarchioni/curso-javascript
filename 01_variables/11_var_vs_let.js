d29dc73e0ff782cf3484cd4f9a01deb577212629
ArielMarchioni <arielmarchioni@gmail.com>
2023-05-11 16:45:03 -0300

    Var vs let

---
+++ 01_variables/11_var_vs_let.js
@@ -0,0 +1,16 @@
+// se puede declarar dos veces una variable con var
+
+var campista = "james";
+var campista = "David";
+
+console.log(campista);
+
+
+//con let solo se puede declarar una vez
+let campista = "james";
+let campista = "David";
+
+console.log(campista);
+
+let campista = "james"
+campista = "David"
