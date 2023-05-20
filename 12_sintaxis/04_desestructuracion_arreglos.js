06574224299de536760a55f5fee6e187948a0acf
ArielMarchioni <arielmarchioni@gmail.com>
2023-05-20 18:58:22 -0300

    desestructura con arreglos

---
+++ 12_sintaxis/04_desestructuracion_arreglos.js
@@ -0,0 +1,16 @@
+var a;
+var b;
+var c;
+[a, b,,, c] = [1, 2, 3, 4, 5, 6];
+
+console.log(a);//1
+console.log(b);//2
+console.log(c);//5
+
+var a = 8;
+var b = 6;
+//asignamos el valores entre a y b ;
+[b, a] = [a, b
+
+console.log("a: " + a);//6
+console.log("b: " + b);//	8
